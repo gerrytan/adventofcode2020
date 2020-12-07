2d7d3989fd7de5313fd1fa742e4a461857c9fe0e
Gerry Tan <gerry.g.tan@gmail.com>
2020-12-07 23:31:10 +1100

    day7

---
+++ day07/part2.js
@@ -0,0 +1,84 @@
+// https://adventofcode.com/2020/day/7
+const readline = require("readline");
+
+// https://nodejs.org/docs/latest-v14.x/api/readline.html
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+// map of contain relationship
+// for example:
+// {
+//   "light red": { "bright white": true, "muted yellow": true },
+//   "faded blue": {}, // empty obj means contains no other bag
+// };
+const contains = {};
+
+rl.on("line", (line) => {
+  const colors = line // 'light red bags contain 1 bright white bag, 2 muted yellow bags.'
+    .split(/ contain |, /) // [ 'light red bags', '1 bright white bag', '2 muted yellow bags.' ]
+    .map((token) => token.replace(/ bags?\.?$/, "")); // [ 'light red', '1 bright white', '2 muted yellow' ]
+  // .map((token) => token.replace(/^\d+ /, "")); // [ 'light red', 'bright white', 'muted yellow' ]
+
+  const parent = colors[0];
+  const children = {};
+  colors
+    .slice(1)
+    .filter((child) => child !== "no other")
+    .forEach((child) => {
+      // sample value of child: '1 bright white'
+      const [
+        countStr, // '1'
+        ...colorNames // ['bright', 'white']
+      ] = child.split(/\s+/);
+      children[colorNames.join(" ")] = parseInt(countStr);
+    });
+  contains[parent] = children;
+});
+
+rl.on("close", () => {
+  // bag colour that can contain shiny gold bag
+  // for example: { "muted yellow": true };
+  // const canContainCache = {};
+
+  // Can the given color contain 'shiny gold' ?
+  // const canContain = (color) => {
+  //   if (color === "shiny gold" || canContainCache[color] === true) {
+  //     return true;
+  //   }
+  //   const children = contains[color]; // { "bright white": true, "muted yellow": true }
+  //   const childrenKeys = Object.keys(children); // [ "bright white", "muted yellow" ]
+  //   for (let i = 0; i < childrenKeys.length; i++) {
+  //     const child = childrenKeys[i];
+  //     if (canContain(child)) {
+  //       canContainCache[color] = true;
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // };
+
+  // Object.keys(contains).forEach((color) => {
+  //   if (color !== "shiny gold" && canContain(color)) {
+  //     canContainCache[color] = true;
+  //   }
+  // });
+
+  // console.log(Object.keys(canContainCache).length);
+
+  const count = (color) => {
+    const children = contains[color];
+    if (children === {}) {
+      return 1;
+    }
+    let descendantCount = 0;
+    for (const [childColor, childCount] of Object.entries(children)) {
+      descendantCount += childCount + childCount * count(childColor);
+    }
+    return descendantCount;
+  };
+
+  console.log(count("shiny gold"));
+});
